5f8fd4056901bb005a03b31d2abbaed5ffb3bc4a
Vu Nguyen Tien <gtvvunguye@gmail.com>
2023-05-26 10:13:46 +0700

    fix mistake at D problem

--- Codeforces/Educational Codeforces Round 149 (div.2)/D/template.java
+++ Codeforces/Educational Codeforces Round 149 (div.2)/D/template.java
@@ -62,106 +62,56 @@ public class template {
                pw.println(-1);
                continue;
            }
-           Pair<Integer, Integer> data[]=new Pair[n];
-           if(s.charAt(0)=='('){
-               data[0]=new Pair<>(1, 0);
-           }else{
-               data[0]=new Pair<>(0, 1);
-           }
-           for(int i=1; i<n; i++){
+           Vector<Integer> a=new Vector<>();
+           int[] ans=new int[n];
+           Arrays.fill(ans, 2);
+           for(int i=0; i<n; i++){
                if(s.charAt(i)=='('){
-                   data[i]=new Pair<>(data[i-1].getFirst()+1, data[i-1].getSecond());
+                   a.add(i);
                }else{
-                   data[i]=new Pair<>(data[i-1].getFirst(), data[i-1].getSecond()+1);
+                   if(!a.isEmpty()){
+                       ans[i]=1;
+                       ans[a.lastElement()]=1;
+                       a.removeElementAt(a.size()-1);
+                   }
                }
            }
            int cnt=0;
-           int[] ans=new int[n];
-           int now=0;
-               if(s.charAt(0)=='('){
-                   for(int i=0; i<n; i++){
-                       if(data[i].getFirst()==(data[n-1].getSecond()-data[i].getSecond())){
-                           cnt=data[i].getFirst();
-                           for(int j=0; j<=i; j++){
-                               if(s.charAt(j)=='('){
-                                   ans[j]=1;
-                               }
-                           }
-                           for(int j=i+1; j<n; j++){
-                               if(s.charAt(j)==')'){
-                                   ans[j]=1;
-                               }
-                           }
-                           break;
+           for(int i=0; i<n; i++){
+               if(ans[i]==2){
+                   ++cnt;
+                   break;
+               }
+           }
+           if(cnt>0){
+               a.removeAllElements();
+               Arrays.fill(ans, 2);
+               for(int i=0; i<n; i++){
+                   if(s.charAt(i)==')'){
+                       a.add(i);
+                   }else{
+                       if(!a.isEmpty()){
+                           ans[i]=1;
+                           ans[a.lastElement()]=1;
+                           a.removeElementAt(a.size()-1);
                        }
                    }
                }
-            if(s.charAt(0)==')'){
-                for(int i=0; i<n; i++){
-                    if(data[i].getSecond()==(data[n-1].getFirst()-data[i].getFirst())){
-                        cnt=data[i].getSecond();
-                        for(int j=0; j<=i; j++){
-                            if(s.charAt(j)==')'){
-                                ans[j]=1;
-                            }
-                        }
-                        for(int j=i+1; j<n; j++){
-                            if(s.charAt(j)=='('){
-                                ans[j]=1;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-            now=0;
-            while(now<n){
-                if(ans[now]==0){
-                    break;
-                }else{
-                    ++now;
-                }
-            }
-            if(now<n){
-                int need=(n-cnt*2)/2;
-                if(s.charAt(now)=='('){
-                    int i=now;
-                    while(need>0){
-                        if(s.charAt(i)=='(' && ans[i]==0){
-                            ans[i]=2;
-                            --need;
-                        }
-                        ++i;
-                    }
-
-                }else{
-                    int i=now;
-                    while(need>0){
-                        if(s.charAt(i)==')' && ans[i]==0){
-                            ans[i]=2;
-                            --need;
-                        }
-                        ++i;
-                    }
-                }
-            }
-            if(now>=n){
-                pw.println(1);
-                for(int i=0; i<n; i++){
-                    pw.print(1+" ");
-                }
-                pw.println();
-            }else{
-                pw.println(2);
-                for(int i=0; i<n; i++){
-                    if(ans[i]>0){
-                        pw.print(ans[i]+" ");
-                    }else{
-                        pw.print(2+" ");
-                    }
-                }
-                pw.println();
-            }
+           }
+           int maxx=0;
+           for(int i=0; i<n; i++){
+               if(ans[i]==2){
+                   maxx=2;
+                   break;
+               }else{
+                   maxx=1;
+               }
+           }
+           pw.println(maxx);
+           for(int i=0; i<n; i++){
+               pw.print(ans[i]+" ");
+           }
+           pw.println();
         }
         pw.close(); // flushes the output once printing is done
     }
