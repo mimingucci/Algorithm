cc68e7a81af09017ff92ba8998da822fada307f9
Vu Nguyen Tien <gtvvunguye@gmail.com>
2023-09-05 19:28:31 +0700

    z function

---
+++ String/Z function/zfunction.cpp
@@ -0,0 +1,33 @@
+/*
+author: mimingucci
+created on: 9/5/2023 7:25:11 PM
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
