b5d4f90d074a70b506646d789565b8819a4bb50a
Vu Nguyen Tien <gtvvunguye@gmail.com>
2024-03-20 19:24:54 +0700

    manacher diff ver

---
+++ String/Manacher/manacher_version1.cpp
@@ -0,0 +1,33 @@
+vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 1, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = max(int(0), min(r - i, p[l + (r - i)]));
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i], r = i + p[i];
+        }
+    }
+    return vector<int>(begin(p) + 1, end(p) - 1);
+}
+
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    return vector<int>(begin(res) + 1, end(res) - 1);
+}
+
+/*
+0-base
+we want to check whether substring s[l...r] is palindrome: 
+auto v=manacher(s);
+if v[l+r]<(r-l+1) -> not palindrome
+if v[l+r]>=(r-l+1) -> is a palindrome
+*/
